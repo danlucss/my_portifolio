c88ecef2e1fadaa5573800ea26ac03af8224f2d8
Daniel Lucas <daniellucas.bio@gmail.com>
2022-08-16 00:49:30 -0300

    Add Sidebar and star particles effect

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -31,77 +31,120 @@ function Particle() {
                     {
                         background: {
                             color: {
-                                value: "#ccc",
-                            },
+                                value: "#000000"
+                            }
                         },
-                        fpsLimit: 120,
-                        interactivity: {
-                            events: {
-                                onClick: {
-                                    enable: true,
-                                    mode: "repulse",
-                                },
-                                onHover: {
-                                    enable: true,
-                                    mode: "attract",
-                                    parallax: { enable: false, force: 60, smooth: 10 }
-                                },
-                                resize: true,
+                        "particles": {
+                            "number": {
+                                "value": 355,
+                                "density": {
+                                    "enable": true,
+                                    "value_area": 789.1476416322727
+                                }
                             },
-                            modes: {
-                                push: {
-                                    quantity: 4,
-                                },
-                                repulse: {
-                                    distance: 200,
-                                    duration: 0.4,
-                                },
-                                attract: { distance: 200, duration: 0.4, factor: 5 }
+                            "color": {
+                                "value": "#ffffff"
                             },
+                            "shape": {
+                                "type": "circle",
+                                "stroke": {
+                                    "width": 0,
+                                    "color": "#000000"
+                                },
+                                "polygon": {
+                                    "nb_sides": 5
+                                },
+                                "image": {
+                                    "src": "img/github.svg",
+                                    "width": 100,
+                                    "height": 100
+                                }
+                            },
+                            "opacity": {
+                                "value": 0.48927153781200905,
+                                "random": false,
+                                "anim": {
+                                    "enable": true,
+                                    "speed": 0.2,
+                                    "opacity_min": 0,
+                                    "sync": false
+                                }
+                            },
+                            "size": {
+                                "value": 2,
+                                "random": true,
+                                "anim": {
+                                    "enable": true,
+                                    "speed": 2,
+                                    "size_min": 0,
+                                    "sync": false
+                                }
+                            },
+                            "line_linked": {
+                                "enable": false,
+                                "distance": 150,
+                                "color": "#ffffff",
+                                "opacity": 0.4,
+                                "width": 1
+                            },
+                            "move": {
+                                "enable": true,
+                                "speed": 0.2,
+                                "direction": "none",
+                                "random": true,
+                                "straight": false,
+                                "out_mode": "out",
+                                "bounce": false,
+                                "attract": {
+                                    "enable": false,
+                                    "rotateX": 600,
+                                    "rotateY": 1200
+                                }
+                            }
                         },
-                        particles: {
-                            color: {
-                                value: "#f000",
-                            },
-                            links: {
-                                color: "#f000",
-                                distance: 200,
-                                enable: true,
-                                opacity: 0.4,
-                                width: 3,
-                            },
-                            collisions: {
-                                enable: true,
-                            },
-                            move: {
-                                directions: "none",
-                                enable: true,
-                                outModes: {
-                                    default: "bounce",
+                        "interactivity": {
+                            "detect_on": "canvas",
+                            "events": {
+                                "onhover": {
+                                    "enable": true,
+                                    "mode": "bubble"
                                 },
-                                random: false,
-                                speed: 3,
-                                straight: false,
-                            },
-                            number: {
-                                density: {
-                                    enable: true,
-                                    area: 1100,
+                                "onclick": {
+                                    "enable": true,
+                                    "mode": "push"
                                 },
-                                value: 40,
+                                "resize": true
                             },
-                            opacity: {
-                                value: 0.6,
-                            },
-                            shape: {
-                                type: "circle",
-                            },
-                            size: {
-                                value: { min: 1, max: 5 },
-                            },
+                            "modes": {
+                                "grab": {
+                                    "distance": 400,
+                                    "line_linked": {
+                                        "opacity": 1
+                                    }
+                                },
+                                "bubble": {
+                                    "distance": 83.91608391608392,
+                                    "size": 1,
+                                    "duration": 3,
+                                    "opacity": 1,
+                                    "speed": 3
+                                },
+                                "repulse": {
+                                    "distance": 200,
+                                    "duration": 0.4
+                                },
+                                "push": {
+                                    "particles_nb": 4
+                                },
+                                "remove": {
+                                    "particles_nb": 2
+                                }
+                            }
                         },
-                        detectRetina: true,
-                    }}
+                        "retina_detect": true
+                    }
+                }
+
             />
         </>
     )
